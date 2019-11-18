66dcc0aa13980e1b72fe067b1ef8afd8e446a5bf
TJShae <tj@tjshae.com>
2019-11-18 15:06:06 +0000

    Prints

--- Sources/Elasticsearch/Client/ElasticsearchClient+Search.swift
+++ Sources/Elasticsearch/Client/ElasticsearchClient+Search.swift
@@ -25,8 +25,10 @@ extension ElasticsearchClient {
             return worker.future(error: error)
         }
         let url = ElasticsearchClient.generateURL(path: "/\(index)/_search", routing: routing)
+      print("sending: \(url)")
         return send(HTTPMethod.POST, to: url.string!, with: body).map(to: SearchResponse.self) {jsonData in
-          
+          print("sent: \(url)")
+
             let decoder = JSONDecoder()
             if let aggregations = query.aggs {
                 if aggregations.count > 0 {
