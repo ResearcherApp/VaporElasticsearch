21922955a7190c4f131089d624d01705b28553dd
TJShae <tj@tjshae.com>
2020-10-09 15:40:48 +0100

    Took field in MultiSearchResponse

--- Sources/Elasticsearch/Model/Response/MultiSearchResponse.swift
+++ Sources/Elasticsearch/Model/Response/MultiSearchResponse.swift
@@ -9,15 +9,18 @@ import Foundation
 
 public struct MultiSearchResponse<T: Decodable>: Decodable {
   
+  public let took: Int
   public let responses: [SearchResponse<T>]
   
   enum CodingKeys: String, CodingKey {
+    case took
     case responses
   }
   
   public init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
     
+    self.took = try container.decode(Int.self, forKey: .took)
     self.responses = try container.decode([SearchResponse<T>].self, forKey: .responses)
   }
 }
