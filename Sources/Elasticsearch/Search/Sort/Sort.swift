bd9477c139866305f288c9ec176eb5c4a41e25d7
TJShae <tj@tjshae.com>
2020-10-12 14:32:37 +0100

    Nested needs to be in the Sort encoding

--- Sources/Elasticsearch/Search/Sort/Sort.swift
+++ Sources/Elasticsearch/Search/Sort/Sort.swift
@@ -1,39 +1,44 @@
 import Foundation
 
 public struct Sort: Codable {
-    let key: String
+  let key: String
   let scoreMode: String?
-    let order: SortOrder
-
+  let order: SortOrder
+  let nested: Sort.Nested?
+  
   public init(_ key: String, scoreMode: String? = nil, _ order: SortOrder, nested: Sort.Nested? = nil) {
-        self.key = key
+    self.key = key
     self.scoreMode = scoreMode
-        self.order = order
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: DynamicKey.self)
-        let key = container.allKeys[0]
-        self.key = key.stringValue
-
-        let valuesContainer = try container.nestedContainer(keyedBy: InnerKeys.self, forKey: key)
-      self.scoreMode = try valuesContainer.decodeIfPresent(String.self, forKey: .scoreMode)
-        self.order = try valuesContainer.decode(SortOrder.self, forKey: .order)
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: DynamicKey.self)
-        var valuesContainer = container.nestedContainer(keyedBy: InnerKeys.self, forKey: DynamicKey(stringValue: key)!)
-
-      try valuesContainer.encodeIfPresent(scoreMode, forKey: .scoreMode)
-        try valuesContainer.encode(order, forKey: .order)
-    }
-
-    enum InnerKeys: String, CodingKey {
-      case scoreMode = "mode"
-        case order
-    }
-
+    self.order = order
+    self.nested = nested
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: DynamicKey.self)
+    let key = container.allKeys[0]
+    self.key = key.stringValue
+    
+    let valuesContainer = try container.nestedContainer(keyedBy: InnerKeys.self, forKey: key)
+    self.scoreMode = try valuesContainer.decodeIfPresent(String.self, forKey: .scoreMode)
+    self.order = try valuesContainer.decode(SortOrder.self, forKey: .order)
+    self.nested = try valuesContainer.decode(Sort.Nested?.self, forKey: .nested)
+  }
+  
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: DynamicKey.self)
+    var valuesContainer = container.nestedContainer(keyedBy: InnerKeys.self, forKey: DynamicKey(stringValue: key)!)
+    
+    try valuesContainer.encodeIfPresent(scoreMode, forKey: .scoreMode)
+    try valuesContainer.encode(order, forKey: .order)
+    try valuesContainer.encodeIfPresent(nested, forKey: .nested)
+  }
+  
+  enum InnerKeys: String, CodingKey {
+    case scoreMode = "mode"
+    case order
+    case nested
+  }
+  
   
   
   public struct Nested: Codable {
@@ -68,7 +73,8 @@ public struct Sort: Codable {
       self.filter = try container.decode(AnyQueryElement?.self, forKey: .filter)?.base
     }
   }
-
+  
 }
+
 
 
